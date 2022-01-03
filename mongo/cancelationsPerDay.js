1d0c30e44c8d3064cec899bd50e90738d95d3592
aimproxy <diasmicaelandre@gmail.com>
2022-01-03 19:21:06 +0000

    Add cancelations #16

---
+++ mongo/cancelationsPerDay.js
@@ -0,0 +1,11 @@
+db.getSiblingDB("SantaDB").getCollection("Bookings").aggregate([
+    {
+        $addFields: {"Cancelations": {$sum: 1}}
+    },
+    {
+        $match: {"Canceled": {$eq: true}}
+    },
+    {
+        $project: {"ScheduleDate": 1, "Cancelations": 1, "_id": 0}
+    }
+])
