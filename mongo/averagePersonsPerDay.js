8485f59ef48641ae3475283600d895d786588531
aimproxy <diasmicaelandre@gmail.com>
2022-01-04 22:59:40 +0000

    Add Average Persons Per Day #22

---
+++ mongo/averagePersonsPerDay.js
@@ -0,0 +1,15 @@
+db.getSiblingDB("SantaDB").getCollection("Bookings").aggregate([
+  {
+    $group: {
+      _id: { ScheduleDate: "$ScheduleDate" },
+      average: { $avg: "$NumberOfMembers" }
+    }
+  },
+  {
+    $project: {
+        _id: 0,
+        ScheduleDate: "$_id.ScheduleDate",
+        AverageOfPersons: "$average"
+    }
+  }
+])
