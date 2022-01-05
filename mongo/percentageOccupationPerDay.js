a45ada530579e6da0d0c4247c4ce89d4b3bcfac3
aimproxy <diasmicaelandre@gmail.com>
2022-01-05 15:11:53 +0000

    Add percentageOccupationPerDay.js #14

---
+++ mongo/percentageOccupationPerDay.js
@@ -0,0 +1,10 @@
+db.getSiblingDB("SantaDB").getCollection("Bookings").aggregate([
+    {
+        $group: {
+            _id: "$ScheduleDate",
+            percentage: {
+                $sum: { $multiply: [{ $divide: [1, 50] }, 100]}
+            }
+        }
+    }
+])
